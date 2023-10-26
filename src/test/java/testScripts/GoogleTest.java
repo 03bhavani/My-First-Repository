69f0cc803560c11ef54783563f20701c0ccdcbc0
03bhavani <149026239+03bhavani@users.noreply.github.com>
2023-10-26 11:59:07 -0700

    Update GoogleTest.java

--- src/test/java/testScripts/GoogleTest.java
+++ src/test/java/testScripts/GoogleTest.java
@@ -1,13 +1,13 @@
 package testScripts;
 
 
-//import org.openqa.selenium.By;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.Parameters;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import common.reusable;
 import pageObject.Google_Page;
